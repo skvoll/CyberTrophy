e0b5f8092c0d4c2ba2d243c8573535d624f8949a
skvoll <skvoll.skvoll@gmail.com>
2017-12-22 12:53:11 +0300

    31th

--- app/src/main/java/io/github/skvoll/cybertrophy/AchievementFragment.java
+++ app/src/main/java/io/github/skvoll/cybertrophy/AchievementFragment.java
@@ -1,12 +1,15 @@
 package io.github.skvoll.cybertrophy;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import io.github.skvoll.cybertrophy.data.AchievementModel;
@@ -14,6 +17,7 @@ import io.github.skvoll.cybertrophy.data.AchievementModel;
 public class AchievementFragment extends Fragment {
     private static final String KEY_ID = "ID";
 
+    private Context mContext;
     private AchievementModel mAchievementModel;
 
     private ViewGroup mRootView;
@@ -35,7 +39,9 @@ public class AchievementFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if (getContext() == null) {
+        mContext = getContext();
+
+        if (mContext == null) {
             return;
         }
 
@@ -49,7 +55,7 @@ public class AchievementFragment extends Fragment {
         }
 
         Long id = bundle.getLong(KEY_ID);
-        mAchievementModel = AchievementModel.getById(getContext().getContentResolver(), id);
+        mAchievementModel = AchievementModel.getById(mContext.getContentResolver(), id);
 
         if (mAchievementModel == null) {
             throw new IllegalArgumentException("Achievement id is missing");
@@ -61,20 +67,48 @@ public class AchievementFragment extends Fragment {
                              Bundle savedInstanceState) {
         mRootView = (ViewGroup) inflater.inflate(R.layout.fragment_achievement, container, false);
 
-        ImageView ivIcon = mRootView.findViewById(R.id.iv_icon);
-        TextView tvName = mRootView.findViewById(R.id.tv_name);
-        TextView tvDescription = mRootView.findViewById(R.id.tv_description);
+        final ImageView ivIcon = mRootView.findViewById(R.id.iv_icon);
+        final TextView tvName = mRootView.findViewById(R.id.tv_name);
+        final Button btnShowInfo = mRootView.findViewById(R.id.btn_show_info);
 
-        String icon = mAchievementModel.isUnlocked() ?
-                mAchievementModel.getIconUrl() : mAchievementModel.getIconGrayUrl();
+        final LinearLayout mAchievementInfo = mRootView.findViewById(R.id.ll_achievement_info);
+        final TextView tvDescription = mRootView.findViewById(R.id.tv_description);
 
-        GlideApp.with(getContext()).load(icon)
-                .placeholder(R.drawable.achievement_icon_empty)
-                .into(ivIcon);
+        final String description = mAchievementModel.getDescription() != null
+                ? mAchievementModel.getDescription()
+                : mContext.getResources().getString(R.string.empty_achievement_description);
 
-        tvName.setText(mAchievementModel.getName());
-        if (mAchievementModel.getDescription() != null) {
-            tvDescription.setText(mAchievementModel.getDescription());
+        if (mAchievementModel.isUnlocked() || !mAchievementModel.isHidden()) {
+            String icon = mAchievementModel.isUnlocked() ?
+                    mAchievementModel.getIconUrl() : mAchievementModel.getIconGrayUrl();
+
+            GlideApp.with(mContext).load(icon)
+                    .placeholder(R.drawable.achievement_icon_empty)
+                    .into(ivIcon);
+            tvName.setText(mAchievementModel.getName());
+            tvDescription.setText(description);
+        } else {
+            GlideApp.with(mContext).load(R.drawable.achievement_icon_hidden)
+                    .placeholder(R.drawable.achievement_icon_empty)
+                    .into(ivIcon);
+            tvName.setText(R.string.achievement_title_hidden);
+
+            mAchievementInfo.setVisibility(View.GONE);
+            btnShowInfo.setVisibility(View.VISIBLE);
+
+            btnShowInfo.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    GlideApp.with(mContext).load(mAchievementModel.getIconGrayUrl())
+                            .placeholder(R.drawable.achievement_icon_empty)
+                            .into(ivIcon);
+                    tvName.setText(mAchievementModel.getName());
+                    tvDescription.setText(description);
+
+                    mAchievementInfo.setVisibility(View.VISIBLE);
+                    btnShowInfo.setVisibility(View.GONE);
+                }
+            });
         }
 
         return mRootView;
