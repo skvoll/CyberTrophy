f07d5a2e9374c9066badd4626f492df41d544d2f
skvoll <skvoll.skvoll@gmail.com>
2017-12-27 16:20:29 +0300

    34th

--- app/src/main/java/io/github/skvoll/cybertrophy/AchievementFragment.java
+++ app/src/main/java/io/github/skvoll/cybertrophy/AchievementFragment.java
@@ -1,8 +1,12 @@
 package io.github.skvoll.cybertrophy;
 
+import android.content.ActivityNotFoundException;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.customtabs.CustomTabsIntent;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,12 +17,14 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import io.github.skvoll.cybertrophy.data.AchievementModel;
+import io.github.skvoll.cybertrophy.data.GameModel;
 
 public class AchievementFragment extends Fragment {
     private static final String KEY_ID = "ID";
 
     private Context mContext;
     private AchievementModel mAchievementModel;
+    private GameModel mGameModel;
 
     private ViewGroup mRootView;
 
@@ -60,6 +66,8 @@ public class AchievementFragment extends Fragment {
         if (mAchievementModel == null) {
             throw new IllegalArgumentException("Achievement id is missing");
         }
+
+        mGameModel = GameModel.getByAppId(mContext.getContentResolver(), mAchievementModel.getAppId());
     }
 
     @Override
@@ -70,6 +78,10 @@ public class AchievementFragment extends Fragment {
         final ImageView ivIcon = mRootView.findViewById(R.id.iv_icon);
         final TextView tvName = mRootView.findViewById(R.id.tv_name);
         final Button btnShowInfo = mRootView.findViewById(R.id.btn_show_info);
+
+        final Button btnGuidesSteam = mRootView.findViewById(R.id.btn_guides_steam);
+        final Button btnGuidesYoutube = mRootView.findViewById(R.id.btn_guides_youtube);
+        final Button btnGuidesGoogle = mRootView.findViewById(R.id.btn_guides_google);
 
         final LinearLayout mAchievementInfo = mRootView.findViewById(R.id.ll_achievement_info);
         final TextView tvDescription = mRootView.findViewById(R.id.tv_description);
@@ -87,6 +99,27 @@ public class AchievementFragment extends Fragment {
                     .into(ivIcon);
             tvName.setText(mAchievementModel.getName());
             tvDescription.setText(description);
+
+            btnGuidesSteam.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    guidesSearchSteam(mGameModel.getAppId(), mAchievementModel.getName());
+                }
+            });
+
+            btnGuidesYoutube.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    guidesSearchYoutube(mGameModel.getName(), mAchievementModel.getName());
+                }
+            });
+
+            btnGuidesGoogle.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    guidesSearchGoogle(mGameModel.getName(), mAchievementModel.getName());
+                }
+            });
         } else {
             GlideApp.with(mContext).load(R.drawable.achievement_icon_hidden)
                     .placeholder(R.drawable.achievement_icon_empty)
@@ -112,5 +145,42 @@ public class AchievementFragment extends Fragment {
         }
 
         return mRootView;
+    }
+
+    private void guidesSearchSteam(Long appId, String achievementName) {
+        openChromeCustomTab(Uri.parse(
+                "http://steamcommunity.com/app/" + appId + "/guides/?searchText=" + achievementName + "&browsefilter=trend&browsesort=creationorder&requiredtags%5B%5D=Achievements"
+        ));
+    }
+
+    private void guidesSearchYoutube(String gameName, String achievementName) {
+        String query = gameName + " " + achievementName + " achievement";
+
+        try {
+            Intent intent = new Intent(Intent.ACTION_SEARCH);
+            intent.setPackage("com.google.android.youtube");
+            intent.putExtra("query", query);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+            startActivity(intent);
+        } catch (ActivityNotFoundException e) {
+            openChromeCustomTab(Uri.parse(
+                    "https://www.youtube.com/results?search_query=" + query
+            ));
+        }
+    }
+
+    private void guidesSearchGoogle(String gameName, String achievementName) {
+        String query = gameName + " " + achievementName + " achievement";
+
+        openChromeCustomTab(Uri.parse(
+                "https://www.google.ru/search?q=" + query
+        ));
+    }
+
+    private void openChromeCustomTab(Uri uri) {
+        CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+        CustomTabsIntent customTabsIntent = builder.build();
+        customTabsIntent.launchUrl(mContext, uri);
     }
 }
